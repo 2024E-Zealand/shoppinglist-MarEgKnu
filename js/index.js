6909b6ac9aac7643234d8cd2a059cbd3b5d38498
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-10-01 15:23:57 +0200

    solved the rest

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
-
+var nextId = 4
 
 function NewLiElement(className, id, content) 
 {
@@ -25,12 +25,49 @@ function DeleteElement(element)
 {
     element.remove()
 }
+function ChangeText(element, newText) 
+{
+    element.textContent = newText
+}
+function NoOfChildElements(element) 
+{
+    return element.childElementCount
+}
+function MoveElement(elementToMove, from, to) 
+{
+    to.append(elementToMove)
+}
 
+function addGood() 
+{
+    let className = "unhealthy"
+    let destList = "listUnhealthy"
+    let isHealthy = document.getElementById("isHealthy").checked
+    let inputText = document.getElementById("goodsInput").value
+    if (isHealthy == true) 
+    {
+        className = "healthy"
+        destList = "list"
+    }
+    let newElement = NewLiElement(className, nextId.toString(), inputText) 
+    nextId = nextId + 1
+    newElement.addEventListener("click", function() 
+    {
+        newElement.hidden = true
+    })
+    AppendListElement(newElement, destList)
+
+}
+
+document.getElementById("addGoods").addEventListener("click", addGood)
 
 
 
 AppendListElement(NewLiElement("", "haha", "lmao"),"list" )
 
-let e = document.getElementById("haha")
-ChangeClassAttr(e, "LOOOOOL")
-DeleteElement(e)
+let from = document.getElementById("list")
+let to = document.getElementById("listUnhealthy")
+let unhealthy = document.getElementsByClassName("unhealthy")
+for (var i = 0; i < unhealthy.length; i++) {
+    MoveElement(unhealthy[i], from, to); 
+}
