1c1d6255864a39e46db6888c54e29b684c5adecf
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-10-01 13:29:50 +0200

    create list item func

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+
+function NewLiElement(className, id, content) 
+{
+    let element = document.createElement("li")
+    element.setAttribute("class", className)
+    element.setAttribute("id", id)
+    element.append(document.createTextNode(content))
+    return element
+
+}
+
+let element = NewLiElement("boomkin", "1", "hehhehe")
+
+console.log(element)
